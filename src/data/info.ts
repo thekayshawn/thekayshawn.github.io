97366843d7ea2318e1ee14a277f74b7d8d43c1e5
kashan-ahmad <kashanahmed@hwryk.com>
2023-02-22 11:54:14 +0500

    feat: :fire: added content@v2.0.0

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,47 +1,55 @@
 export const info = {
   baseUrl: "https://kashan-ahmad.github.io",
   name: "Kashan Ahmad",
-  jobDescription: "Software Engineer",
+  jobDescription: "Frontend Web Developer",
   about:
     "I'm a dynamic and proactive software engineer with a proven track record of success in the development of exceptional software systems. Right now, I'm developing a seamless dashboard system at TheTutor.",
 
   experience: [
     {
-      name: "Lead Engineer @TheTutor.me",
+      name: "Frontend Web Developer @TheTutor.me",
       location: "Dubai, UAE",
       startDate: "June 2022",
       endDate: "Present",
       description: [
-        "Wrote, managed, and reviewed enterprise-level React code for the EdTech platform. Developed a maintainable user dashboard and a drag-and-drop content builder.",
+        `Leading a team of efficient individuals through writing, managing, and reviewing enterprise-level React and TypeScript code for the EdTech platform. The product was released in October after a comprehensive closed beta-testing session got wrapped up.
+
+        Currently, we're building a user-first dashboard system that allows the tutors and their students to seamlessly manage their assets and purchase/sell the products. That's not all, a drag-and-drop content builder tool allows the tutors to create complex presentations, learning materials, and real-time quizzes, thanks to React and TypeScript.`,
       ],
     },
 
     {
-      name: "Engineering Manager @Devshot",
-      location: "Birmingham, UK",
+      name: "Frontend Web Developer @How2Pass",
+      location: "Ontario, Canada",
       startDate: "June 2021",
-      endDate: "June 2022",
+      endDate: "Present",
       description: [
-        "Conducted regular code reviews, lead, and managed a team of talented software engineers, serving as a mentor for the newly acquisitioned talent.",
+        `Writing scalable React code that's leading the product to be used by hundreds of businesses and entrepreneurs to jump-start their careers to new heights.
+        
+        The product has been selling more than ever since we updated it's UI and optimized it for the phones. Currently, we're building a comprehensive comments system as a microfrontend which will, later on, be integrated with the main application, allowing the users to communicate in real-time and let us know their suggestions.`,
       ],
     },
 
     {
-      name: "Software Engineer @BusinessDoor",
-      location: "Birmingham, UK",
+      name: "Full-Stack Web Developer @How2Pass",
+      location: "Ontario, Canada",
       startDate: "June 2020",
       endDate: "June 2021",
       description: [
-        "Wrote scalable React code that lead the product to be used by hundreds of businesses and entrepreneurs to jump start their careers to new heights.",
+        `Re-engineered a PHP5 application to utilize modern architectural and behavioral patterns. Optimized the code base and introduced detachable database support.
+
+        Engineered the website to become one of the leading networking-oriented training companies with real-time simulations and terminal experiences, join us at how2pass.com`,
       ],
     },
     {
       name: "Full-Stack Engineer @How2Pass",
       location: "Ontario, CA",
       startDate: "November 2019",
-      endDate: "Present",
+      endDate: "June 2020",
       description: [
-        "Wrote performant & elegant PHP code, complimented by a sweet blend of TypeScript and SCSS. The end result has been a product loved by thousands of users.",
+        `Updated, developed, and maintained the APIs of an admin dashboard system used by the many admins of How2Pass. Designed and developed eloquent RESTful APIs that allowed the admins to make use of a rigorous CRUD system.
+
+        Wrote an ORM for a testimonials system that allowed the users to add testimonials and the admins to moderate.`,
       ],
     },
   ],
@@ -76,7 +84,7 @@ export const info = {
     email: "mailto:kashan.aech.620@gmail.com",
     github: "https://github.com/kashan-ahmad",
     twitter: "https://twitter.com/kashanAhmad_",
-    linkedin: "https://www.linkedin.com/in/kashan-ahmad-227194243/",
+    linkedin: "https://www.linkedin.com/in/kashoot",
   },
 
   projects: [
@@ -89,7 +97,7 @@ export const info = {
     {
       title: "Content Builder",
       isFeatured: true,
-      liveUrl: "https://content-builder.thetutor.me",
+      liveUrl: "https://contentbuilder.thetutor.me",
       thumbnail: "/assets/images/content-builder.png",
     },
     {
