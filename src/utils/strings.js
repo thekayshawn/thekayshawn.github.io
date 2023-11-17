4f15a3cf062e4e824608a9ecc3e343d6c1f6951c
kashan-ahmad <git@oikashan.com>
2023-11-17 23:59:36 +0500

    feat: added social links

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -2,15 +2,16 @@ export const strings = {
   name: "Kashan Ahmad",
   title: "Senior Frontend Software Engineer",
   summary:
-    "I build beautifully animated yet performant web applications. I specialize in JavaScript, TypeScript, React, Next.js, and Tailwind CSS.",
+    "Building beautifully animated yet performant web applications, I specialize in JavaScript, TypeScript, React, Next.js, and Tailwind CSS.",
   bio: [
     "Been shipping accessible web applications since 2018, Kashan is an intuitive and creative software engineer with a passion for building beautifully animated yet performant web applications.",
     "Hailing from Pakistan, Kashan self-taught himself how to code in JavaScript, TypeScript, and PHP. He ♥ open-source, and has shipped many open-source projects. He is also a huge fan of React, Next.js, and Tailwind CSS.",
   ],
   openToWork: "Open to work",
-  openToWorkLabel:
-    "He's currently looking for new opportunities as a Senior Frontend Software Engineer.",
   openToWorkIcon: "bx:bx-bolt-circle",
+  yearsOfExperience: "Years of experience",
+  yearsOfExperienceCount: 5,
+  yearsOfExperienceIcon: "bx:bx-award",
   experiences: "Experiences",
   experiencesIcon: "bx:bxs-cloud-download",
   projects: "Projects",
@@ -22,8 +23,12 @@ export const strings = {
   contactLabel: "Get in touch",
   contactIcon: "bx:bx-mail-send",
   twitterHandle: "@oikashan",
+  twitterIcon: "bx:bxl-twitter",
   githubUsername: "oikashan",
+  githubIcon: "bx:bxl-github",
   linkedInUsername: "oikashani",
+  linkedInIcon: "bx:bxl-linkedin",
   website: "https://oikashan.com",
+  websiteIcon: "bx:bx-world",
   ogImgAlt: "Kashan's Homepage",
 };
