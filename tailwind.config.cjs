e276e24ad77ee7f118cd8484bc38140258cee914
kashan-ahmad <git@oikashan.com>
2023-11-18 12:42:59 +0500

    feat: finalized index page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Bricolage Grotesque Variable", ...defaultTheme.fontFamily.sans],
+      },
+      zIndex: {
+        navbar: 100,
       },
     },
   },
