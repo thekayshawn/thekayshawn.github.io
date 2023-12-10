509e924b5d4bab8573cddf11b1394bbde1708adb
thekayshawn <kashanahmad.me@gmail.com>
2023-12-11 01:43:01 +0500

    fix: fixed og titles as well

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://kashanahmad.me",
   integrations: [tailwind(), mdx(), sitemap()],
 });
