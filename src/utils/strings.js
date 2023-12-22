f066b0d724f125ce55975bbcaf7da0ec242e5f41
Kashan Ahmad <kashanahmad.me@gmail.com>
2023-12-22 07:20:57 +0500

    fix: fixed strings

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,11 +1,10 @@
 export const strings = {
   name: "Kashan Ahmad",
-  title: "Frontend Software Engineer",
+  title: "Senior Software Engineer - Full Stack",
   summary:
-    "A Frontend Software Engineer with over 5 years of experience in startups, agencies, freelancing, and teaching.",
+    "A Full Stack Software Engineer with over 5 years of experience in startups, agencies, freelancing, and teaching.",
   bio: [
-    "Hey there! I've been creating user-friendly websites since 2018. I'm a software engineer from Pakistan who taught myself JavaScript, TypeScript, and PHP. I enjoy working on open-source projects and have completed several. I love exploring the capabilities of the web and am always up for learning new things.",
-    "Recently, I did something pretty cool—I built a 3D spaceship that actually moves in the browser using React, GSAP, and Tailwind CSS. And get this, when you click a link, the ship hyperdrives to a whole new page. It's a fun way to make web browsing a bit more interesting!",
+    "Full Stack Software Engineer experienced with MERN, MEAN, and MEVN stacks, DevOps, Serverless, and Accessibility advocate, shipping open source Startups, frequent learner, and family man.",
   ],
   isOpenToWork: true,
   openToWork: "Open to work",
